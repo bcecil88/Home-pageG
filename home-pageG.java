a32004000f2bf8327f4d22a3826f3c1b748161d3
bcecil88 <bcecil88@gmail.com>
2016-08-15 21:32:39 -0400

    Looping
    Until value passes

--- home-pageG.java
+++ home-pageG.java
@@ -11,22 +11,27 @@ import java.io.Console;
 public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
-      String ageAsString = console.readLine("How old are you?  ");
-      int age = Integer.parseInt(ageAsString);
+			String ageAsString = console.readLine("How old are you?  ");
+			int age = Integer.parseInt(ageAsString);
   if (age < 13) {
     console.printf("Sorry, you must be at least 13 to use this program.\n");
     System.exit(0);
   }
       String name = console.readLine("Enter your name:  ");
       String adjective = console.readLine("Enter an adjective:  ");
-      String noun = console.readLine("Enter a noun:  ");
-      if (noun.equals("dork")) {
-        console.printf("That language is not allowed. Exiting. \n\n");
-        System.exit(0);
-      }
-      String adverb = console.readLine("Enter an adverb:  ");
-      console.printf("%s is very %s of a %s %s\n", name, adjective, adverb, noun);
+			String noun;
+			boolean isInvalidWord;
+					do {
+					 noun = console.readLine("Enter a noun:  ");
+					 isInvalidWord = (noun.equalsIgnoreCase("dork") || 
+						 												 noun.equalsIgnoreCase("nerd"));
+					if (isInvalidWord) {
+						console.printf("That language is not allowed. Try again. \n\n");
+					}
+					}
+			while (isInvalidWord); {
+			String adverb = console.readLine("Enter an adverb:  ");
+      console.printf("%s is very %s of a %s %s\n", name, adjective, adverb, noun);}
     }
     
 }
-
